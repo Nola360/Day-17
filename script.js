37341080e3ba7e6b647d4f68047005d5d9aca342
nola360 <adaramolajr@gmail.com>
2020-01-17 15:26:28 -0500

    added ES6 Classes

--- script.js
+++ script.js
@@ -1,84 +1,38 @@
-// Person construnctor 
-
-function Person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-}
-
-// Greeting
-Person.prototype.greeting = function () {
-  return `Hello ${this.firstName} ${this.lastName}`;
-}
-
-const person1 = new Person('Penny', 'Kandy');
-
-console.log(person1.greeting());
 
 
-// Customer constructor
-function Customer(firstName, lastName, phone, membership) {
-  // Call() allows another function to be called in context
-  Person.call(this, firstName, lastName);
-  this.phone = phone;
-  this.membership = membership;
-}
+// ES6 CLASSES
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+  }
+  // Method
+  greeting() {
+    return `Hello there ${this.firstName} ${this.lastName} you're a big piece of dog shit!`
+  }
 
+  calculateAge() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1916);
+  }
 
+  getsMarried(newLastName) {
+    this.lastName = newLastName;
+  }
 
-// Inherit Person prototype methods
-Customer.prototype = Object.create(Person.prototype);
-
-
-
-// Make customer.prototype return customer() instead of person()
-Customer.prototype.constructor = Customer;
-
-
-
-// Create customer
-const customer1 = new Customer('Test', 'Averti', '555-555-5555', 'Standard');
-
-console.log(customer1);
-
-// Customer greeting
-Customer.prototype.customerGreeting = function () {
-  return `Hello there ${this.firstName} ${this.lastName} welcome to our business!`
-}
-
-
-console.log(customer1.customerGreeting());
-
-
-
-// Creating Objects
-
-const personPrototypes = {
-  greeting: function () {
-    return `Hello there ${this.firstName} ${this.lastName}`;
-  },
-
-  getsMarried: function (newLastName) {
-    this.lastName = newLastName
+  static addNumbers(x, y) {
+    return x + y;
   }
 }
 
-const mary = Object.create(personPrototypes);
-mary.firstName = 'Mary';
-mary.lastName = 'Williams';
-mary.age = 30;
-
-mary.getsMarried('Sweets')
-
-console.log(mary.greeting());
-
-const james = Object.create(personPrototypes, {
-  firstName: { value: 'James' },
-  lastName: { value: 'Bonds' },
-  age: { value: 46 }
-})
-
-console.log(james.greeting());
-console.log(james);
 
 
+
+const tony = new Person('Tony', 'Taressi', 'April 1, 1916');
+
+tony.getsMarried('Sheffield')
+console.log(Person.addNumbers(1, 4));
+console.log(tony.calculateAge());
 
